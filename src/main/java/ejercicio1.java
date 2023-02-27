9c9df4278499d50262ef814b61137765520178d8
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-02-27 16:00:59 +0100

    Ejercicios comenzados

---
+++ src/main/java/ejercicio1.java
@@ -0,0 +1,43 @@
+//EJERCICIO 1
+
+public class ejercicio1 {
+
+
+    public static void main(String[] args) {
+        double a = 5.5;
+        double b = 8.2;
+
+        double mayor = obtenerMayor(a, b);
+        System.out.println("El mayor valor es: " + mayor);
+
+        int comparacion = compararValores(a, b);
+        if (comparacion == 0) {
+            System.out.println("Los valores son iguales");
+        } else if (comparacion == 1) {
+            System.out.println("El primer valor es mayor");
+        } else {
+            System.out.println("El segundo valor es mayor");
+        }
+
+    }
+
+    public static double obtenerMayor(double x, double y) {
+        if (x > y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    public static int compararValores(double x, double y) {
+        if (x == y) {
+            return 0;
+        } else if (x > y) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+
+}
