87d2239e806a33346acf5d22f480ea9315fbfea0
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-03-01 12:09:36 +0100

    Falta ejercicio 5

--- src/main/java/ejercicio2.java
+++ src/main/java/ejercicio2.java
@@ -1,9 +1,59 @@
+import java.util.Scanner;
+
 //EJERCICIO 2
 public class ejercicio2 {
+
+    private static final int TAMANO_TABLERO = 10;
+    private static char[][] tablero;
     public static void main(String[] args) {
+        tablero = new char[TAMANO_TABLERO][TAMANO_TABLERO];
+        inicializarTablero();
+        mostrarTablero();
+        disparar();
 
     }
-    public static void taller(){
+
+    public static void inicializarTablero(){
+        for (int i = 0; i < TAMANO_TABLERO; i++) {
+            for (int j = 0; j < TAMANO_TABLERO; j++) {
+                tablero[i][j] = '-';
+            }
+        }
 
     }
+
+    public static void mostrarTablero(){
+        for (int i = 0; i < TAMANO_TABLERO; i++) {
+            for (int j = 0; j < TAMANO_TABLERO; j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static int introducirEnteroEntreLimites(int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        int valor;
+        do {
+            System.out.print("Introduce un valor entre " + min + " y " + max + ": ");
+            valor = scanner.nextInt();
+        } while (valor < min || valor > max);
+        return valor;
+    }
+
+    public static void disparar() {
+        int fila = introducirEnteroEntreLimites(0, TAMANO_TABLERO - 1);
+        int columna = introducirEnteroEntreLimites(0, TAMANO_TABLERO - 1);
+        if (tablero[fila][columna] == 'X') {
+            System.out.println("Ya has disparado en esa posición.");
+        } else if (tablero[fila][columna] == 'O') {
+            System.out.println("¡Hundiste un barco!");
+            tablero[fila][columna] = 'X';
+        } else {
+            System.out.println("Agua.");
+            tablero[fila][columna] = 'X';
+        }
+    }
+
 }
--- src/main/java/ejercicio5.java
+++ src/main/java/ejercicio5.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
+import static jdk.internal.org.jline.keymap.KeyMap.display;
+
 public class ejercicio5 {
+
+
 }
+
+
+
